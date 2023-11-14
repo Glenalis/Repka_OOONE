78d6445c51aa5d628a09455bf596bea4e62c0db8
Daniil <Daniil@mail.ru>
2023-11-14 13:49:31 +0500

    jjj

--- DD/DD/Form1.cs
+++ DD/DD/Form1.cs
@@ -16,5 +16,50 @@ namespace DD
         {
             InitializeComponent();
         }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            // Show the open file dialog. if the user clicks OK, load the
+            // picture that user chose.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Clear the picture.
+            pictureBox1.Image = null;
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //If the user selects the Stretch check box,
+            // change the PictureBox's
+            // SizeMode property to "Stretch". If the user clears
+            //the check box, change it to "Normal".
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
     }
 }
